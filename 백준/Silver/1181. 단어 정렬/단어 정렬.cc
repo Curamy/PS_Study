1c45cdd8abe1f8f5168b06e51c6ec3134bcd4dca
Curamy <82808714+Curamy@users.noreply.github.com>
2025-12-29 17:12:30 +0900

    [Silver V] Title: 단어 정렬, Time: 36 ms, Memory: 3856 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool compare(string a, string b) {
+    if (a.length() == b.length()){
+        return a < b;
+    }
+    else {
+        return a.length() < b.length();
+    }
+}
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    int n;
+    cin >> n;
+    string arr[n];
+
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n, compare);
+
+    for (int i = 0; i < n; i++){
+        if (arr[i] == arr[i - 1]){
+            continue;
+        }
+        cout << arr[i] << "\n";
+    }
+    
+    return 0;
+}
