d0d76e9c613919c23af07f60f48e70563d188c38
Curamy <82808714+Curamy@users.noreply.github.com>
2025-12-29 17:19:02 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int correct_quiz = 11;
+    string id = "damaon2016";
+    cout << 11 << endl;
+    cout << id << endl;
+    return 0;
+}
