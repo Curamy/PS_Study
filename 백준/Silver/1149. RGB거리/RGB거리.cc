e33f5994f6e2e039c020bb432990b528257cee4e
Curamy <82808714+Curamy@users.noreply.github.com>
2026-03-05 15:35:31 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int n;
+int cost[1001][3];
+int dp[1001][3];
+int ans;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    ans = 1000000;
+    cin >> n;
+    for (int i = 1; i <= n; i++){
+        cin >> cost[i][0] >> cost[i][1] >> cost[i][2];
+    }
+
+    dp[1][0] = cost[1][0];
+    dp[1][1] = cost[1][1];
+    dp[1][2] = cost[1][2];
+
+    for (int i = 2; i <= n; i++){
+        dp[i][0] = min(dp[i - 1][1] + cost[i][0], dp[i - 1][2] + cost[i][0]);
+        dp[i][1] = min(dp[i - 1][0] + cost[i][1], dp[i - 1][2] + cost[i][1]);
+        dp[i][2] = min(dp[i - 1][0] + cost[i][2], dp[i - 1][1] + cost[i][2]);
+    }
+
+    cout << min(min(dp[n][0], dp[n][1]), dp[n][2]);
+    
+    return 0;
+}
