90ba5ff91352f50d4314f868773391e17525aeb9
Curamy <82808714+Curamy@users.noreply.github.com>
2026-03-23 18:24:53 +0900

    [Gold IV] Title: 플로이드, Time: 20 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -1,14 +1,11 @@
+// 플로이드-워셜 알고리즘을 이용한 풀이
 #include <iostream>
-#include <vector>
-#include <queue>
 #include <algorithm>
 #define INF 99999999
 using namespace std;
 
 int n, m;
-vector<pair<int, int>> buses[101];
-int dijk[101];
-priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+int dist[101][101];
 
 int main() {
     cin.tie(NULL);
@@ -16,45 +13,38 @@ int main() {
     ios_base::sync_with_stdio(false);
 
     cin >> n >> m;
+    fill(dist[0], dist[101], INF);
+
     for (int i = 0; i < m; i++){
         int s, e, w;
         cin >> s >> e >> w;
-        buses[s].push_back({w, e});
+        
+        if (dist[s][e] > w){
+            dist[s][e] = w;
+        }
     }
 
     for (int i = 1; i <= n; i++){
-        fill(dijk, dijk + 101, INF);
-        while(!pq.empty()){
-            pq.pop();
-        }
+        dist[i][i] = 0;
+    }
 
-        dijk[i] = 0;
-        pq.push({0, i});
-        
-        while(!pq.empty()){
-            int weight = pq.top().first;
-            int city = pq.top().second;
-            pq.pop();
-
-            if (dijk[city] != weight){
-                continue;
-            }
-
-            for (auto nxt : buses[city]){
-                if (dijk[nxt.second] > weight + nxt.first){
-                    dijk[nxt.second] = weight + nxt.first;
-                    pq.push({dijk[nxt.second], nxt.second});
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= n; j++){
+            for (int k = 1; k <= n; k++){
+                if (dist[j][k] > dist[j][i] + dist[i][k]){
+                    dist[j][k] = dist[j][i] + dist[i][k];
                 }
             }
         }
+    }
 
-
+    for (int i = 1; i <= n; i++){
         for (int j = 1; j <= n; j++){
-            if (dijk[j] == INF){
+            if (dist[i][j] == INF){
                 cout << "0 ";
             }
             else{
-                cout << dijk[j] << " ";
+                cout << dist[i][j] << " ";
             }
         }
         cout << "\n";
@@ -62,3 +52,71 @@ int main() {
 
     return 0;
 }
+
+
+// 다익스트라 알고리즘을 이용한 풀이
+
+// #include <iostream>
+// #include <vector>
+// #include <queue>
+// #include <algorithm>
+// #define INF 99999999
+// using namespace std;
+
+// int n, m;
+// vector<pair<int, int>> buses[101];
+// int dijk[101];
+// priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+// int main() {
+//     cin.tie(NULL);
+//     cout.tie(NULL);
+//     ios_base::sync_with_stdio(false);
+
+//     cin >> n >> m;
+//     for (int i = 0; i < m; i++){
+//         int s, e, w;
+//         cin >> s >> e >> w;
+//         buses[s].push_back({w, e});
+//     }
+
+//     for (int i = 1; i <= n; i++){
+//         fill(dijk, dijk + 101, INF);
+//         while(!pq.empty()){
+//             pq.pop();
+//         }
+
+//         dijk[i] = 0;
+//         pq.push({0, i});
+        
+//         while(!pq.empty()){
+//             int weight = pq.top().first;
+//             int city = pq.top().second;
+//             pq.pop();
+
+//             if (dijk[city] != weight){
+//                 continue;
+//             }
+
+//             for (auto nxt : buses[city]){
+//                 if (dijk[nxt.second] > weight + nxt.first){
+//                     dijk[nxt.second] = weight + nxt.first;
+//                     pq.push({dijk[nxt.second], nxt.second});
+//                 }
+//             }
+//         }
+
+
+//         for (int j = 1; j <= n; j++){
+//             if (dijk[j] == INF){
+//                 cout << "0 ";
+//             }
+//             else{
+//                 cout << dijk[j] << " ";
+//             }
+//         }
+//         cout << "\n";
+//     }
+
+//     return 0;
+// }
